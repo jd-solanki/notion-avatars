e8959e724b5286e86a92e11d41e81cab0386ca98
JD Solanki <jdsolanki0001@gmail.com>
2024-12-26 18:11:12 +0530

    feat: improved site

--- app/app.vue
+++ app/app.vue
@@ -7,14 +7,21 @@ const config = ref<AvatarConfig>(getRandomAvatarStyle())
 </script>
 
 <template>
-  <div>
+  <div class="min-h-[100dvh] flex flex-col font-bold">
     <NuxtRouteAnnouncer />
-    <!-- <img
-      src="~/assets/svg/avatar/part/face/face-0.svg"
-      alt=""
-    > -->
+    <header class="container flex items-center">
+      <img
+        src="/logo.gif"
+        alt="notion-avatars"
+        class="size-12"
+      >
+      <h1 class="font-semibold text-2xl">
+        Notion Avatars
+      </h1>
+      <HeaderDecoration class="absolute top-0 right-0 w-24 sm:w-28 md:w-36" />
+    </header>
     <!-- TODO: Remove this client only wrapper -->
-    <div class="container">
+    <div class="container my-auto">
       <ClientOnly>
         <div class="w-48 h-48 md:w-72 md:h-72 mx-auto">
           <AvatarPreview :config="config" />
@@ -26,12 +33,14 @@ const config = ref<AvatarConfig>(getRandomAvatarStyle())
           Random
         </Button>
       </div>
+      <Footer />
     </div>
   </div>
 </template>
 
 <style>
-body {
+html {
   font-family: 'Quicksand', sans-serif;
+  background-color: #fffefc;
 }
 </style>
---
+++ app/components/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <footer class="flex flex-col items-center pb-4">
+    <img
+      src="/line.svg"
+      width="125"
+      height="54"
+    >
+    <div class="flex justify-center mt-10">
+      <NuxtLink
+        class="hover:underline"
+        href="https://github.com/jd-solanki/notion-avatars"
+        target="_blank"
+      >
+        GitHub
+      </NuxtLink>
+      <span class="mx-2">·</span>
+      <NuxtLink
+        class="hover:underline"
+        href="https://x.com/me_jd_solanki"
+        target="_blank"
+      >
+        𝕏
+      </NuxtLink>
+    </div>
+    <div class="text-gray-300 mt-3 px-6 text-center">
+      <NuxtLink
+        href="https://abstractlab.gumroad.com/l/noto-avatar"
+        class="hover:underline"
+        target="_blank"
+      >
+        &copy; Illustrations
+      </NuxtLink>
+      designed by
+      <NuxtLink
+        href="https://twitter.com/felix12777"
+        class="hover:underline"
+        target="_blank"
+      >Felix Wong</NuxtLink>
+      and under
+      <NuxtLink
+        class="hover:underline"
+        href="https://creativecommons.org/publicdomain/zero/1.0/"
+        target="_blank"
+      >CC0</NuxtLink>
+      license
+    </div>
+    <p class="text-gray-300">
+      Adapted from <NuxtLink
+        href="https://github.com/Mayandev/notion-avatar/"
+        target="_blank"
+        class="hover:underline"
+      >Notion Avatar</NuxtLink> by <NuxtLink
+        href="https://github.com/Mayandev/"
+        target="_blank"
+        class="hover:underline"
+      >Mayandev</NuxtLink>
+      reimagined and fixed using <NuxtLink
+        href="https://nuxt.com/"
+        class="hover:underline"
+        target="_blank"
+      >Nuxt.js</NuxtLink>.
+    </p>
+  </footer>
+</template>
