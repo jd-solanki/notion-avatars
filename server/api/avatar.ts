825e6a569fb23f6245609679d660199323dbb1fb
JD Solanki <jdsolanki0001@gmail.com>
2024-12-28 16:35:10 +0530

    feat(api): remove unknown query params to avoid dynamically loading non-existing SVG

--- server/api/avatar.ts
+++ server/api/avatar.ts
@@ -1,12 +1,13 @@
 import { useAvatarPreview } from '~/composables/useAvatarPreview'
-import { getAvatarStyle } from '~~/shared/utils'
+import { getAvatarStyle, parseAvatarStyleQueryParams } from '~~/shared/utils'
 
 export default defineEventHandler((event) => {
   const query = getQuery(event)
 
-  const appliedAvatarStyle = getAvatarStyle(query)
+  const appliedAvatarStyle = parseAvatarStyleQueryParams(query)
+  const config = getAvatarStyle(appliedAvatarStyle)
 
-  const { svgString } = useAvatarPreview(appliedAvatarStyle)
+  const { svgString } = useAvatarPreview(config)
 
   // Set the content type for SVG
   setHeader(event, 'Content-Type', 'image/svg+xml')
