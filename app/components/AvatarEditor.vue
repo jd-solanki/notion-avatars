ebc06a438a429c5ca65d3d0dbd260154d17564fc
JD Solanki <jdsolanki0001@gmail.com>
2024-12-28 14:49:13 +0530

    feat: keep style selection open when style is selected

--- app/components/AvatarEditor.vue
+++ app/components/AvatarEditor.vue
@@ -3,42 +3,48 @@ import type { AvatarConfig } from '~~/shared/types'
 
 const config = defineModel<AvatarConfig>('config', { required: true })
 
-const notionAvatarParts = getParts()
+const avatarParts = getParts()
 </script>
 
 <template>
   <section>
-    <p class="font-medium text-lg">
+    <p class="text-lg">
       Choose your style
     </p>
     <div class="flex justify-between my-4">
       <div
-        v-for="(partPreviews, part) in notionAvatarParts"
+        v-for="(partPreviews, part) in avatarParts"
         :key="part"
         class="border-[3px] rounded-lg border-primary"
       >
-        <DropdownMenu>
-          <DropdownMenuTrigger class="flex p-4">
+        <Popover>
+          <PopoverTrigger class="flex p-4">
             <img
-              :src="partPreviews[config[part]]"
+              :src="partPreviews[config[part as keyof AvatarConfig]]"
               :alt="part"
               class="size-8"
             >
-          </DropdownMenuTrigger>
-          <DropdownMenuContent class="grid grid-cols-4 p-4 gap-4 max-h-86">
-            <DropdownMenuItem
+          </PopoverTrigger>
+          <PopoverContent
+            :side-offset="20"
+            class="grid grid-cols-4 p-4 gap-4 max-h-72 overflow-auto border-primary border-[3px]"
+          >
+            <Button
               v-for="(partPreview, index) in partPreviews"
               :key="index"
-              @click="config[part] = index"
+              variant="ghost"
+              class="size-12 py-1 px-2"
+              :class="{ 'ring-[3px] ring-primary': config[part as keyof AvatarConfig] == index }"
+              @click="config[part as keyof AvatarConfig] = index"
             >
               <img
                 :src="partPreview"
                 :alt="`${part}-${index}`"
-                class="size-12"
+                class="size-8"
               >
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            </Button>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   </section>
